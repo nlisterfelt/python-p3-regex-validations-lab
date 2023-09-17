7d022ef3ca8bee273744c244503622f49a64a8c3
Nicole Listerfelt <nlisterfelt@gmail.com>
2023-09-17 18:49:05 -0500

    re

--- lib/regex.py
+++ lib/regex.py
@@ -3,11 +3,14 @@ import re
 # NOTE: There are only a few tests included, so multiple solutions will work.
 # Feel free to encourage students to find oversights and add tests to this lab!
 
-name = r""
+name = r'John Cena|Anya Taylor\-Joy|D\'Angelo'
 name_regex = re.compile(name)
 
-phone_number = r""
+phone_number = r'[5]{10}|555\-555\-5555|\(555\) 555\-5555'
 phone_regex = re.compile(phone_number)
 
-email_address = r""
+email_address = r"john\.cena\@wwe\.com|johncena\@wwe\.com|john\.cena123\@wwe\.com"
 email_regex = re.compile(email_address)
+
+#'[A-Za-z\- \']'
+#"[5\(\)\-]"
